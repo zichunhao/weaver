7e4f08861e5c0a301ad8d67ea376429ab616f918
cmantill <criss.ms7@gmail.com>
2022-02-16 13:54:24 -0800

    update args

--- train.py
+++ train.py
@@ -17,222 +17,105 @@ from utils.logger import _logger, _configLogger
 from utils.dataset import SimpleIterDataset
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--regression-mode",
-    action="store_true",
-    default=False,
-    help="run in regression mode if this flag is set; otherwise run in classification mode",
-)
-parser.add_argument(
-    "-c",
-    "--data-config",
-    type=str,
-    default="data/ak15_points_pf_sv_v0.yaml",
-    help="data config YAML file",
-)
-parser.add_argument("-i", "--data-train", nargs="*", default=[], help="training files")
-parser.add_argument(
-    "-l",
-    "--data-val",
-    nargs="*",
-    default=[],
-    help="validation files; when not set, will use training files and split by `--train-val-split`",
-)
-parser.add_argument(
-    "-t",
-    "--data-test",
-    nargs="*",
-    default=[],
-    help="testing files; supported syntax:"
-    " (a) plain list, `--data-test /path/to/a/* /path/to/b/*`;"
-    " (b) keyword-based, `--data-test: a:/path/to/a/* b:/path/to/b/*`, will produce output_a, output_b;"
-    " (c) split output per N input files, `--data-test: a%10:/path/to/a/*`, will split per 10 input files",
-)
-parser.add_argument(
-    "--data-fraction",
-    type=float,
-    default=1,
-    help="fraction of events to load from each file; for training, the events are randomly selected for each epoch",
-)
-parser.add_argument(
-    "--file-fraction",
-    type=float,
-    default=1,
-    help="fraction of files to load; for training, the files are randomly selected for each epoch",
-)
-parser.add_argument(
-    "--fetch-by-files",
-    action="store_true",
-    default=False,
-    help="When enabled, will load all events from a small number (set by ``--fetch-step``) of files for each data fetching. "
-    "Otherwise (default), load a small fraction of events from all files each time, which helps reduce variations in the sample composition.",
-)
-parser.add_argument(
-    "--fetch-step",
-    type=float,
-    default=0.01,
-    help="fraction of events to load each time from every file (when ``--fetch-by-files`` is disabled); "
-    "Or: number of files to load each time (when ``--fetch-by-files`` is enabled). Shuffling & sampling is done within these events, so set a large enough value.",
-)
-parser.add_argument(
-    "--in-memory",
-    action="store_true",
-    default=False,
-    help="load the whole dataset (and perform the preprocessing) only once and keep it in memory for the entire run",
-)
-parser.add_argument(
-    "--train-val-split", type=float, default=0.8, help="training/validation split fraction"
-)
-parser.add_argument(
-    "--demo",
-    action="store_true",
-    default=False,
-    help="quickly test the setup by running over only a small number of events",
-)
-parser.add_argument(
-    "--lr-finder",
-    type=str,
-    default=None,
-    help="run learning rate finder instead of the actual training; format: ``start_lr, end_lr, num_iters``",
-)
-parser.add_argument(
-    "--tensorboard",
-    type=str,
-    default=None,
-    help="create a tensorboard summary writer with the given comment",
-)
-parser.add_argument(
-    "--tensorboard-custom-fn",
-    type=str,
-    default=None,
-    help="the path of the python script containing a user-specified function `get_tensorboard_custom_fn`, "
-    "to display custom information per mini-batch or per epoch, during the training, validation or test.",
-)
-parser.add_argument(
-    "-n",
-    "--network-config",
-    type=str,
-    default="networks/particle_net_pfcand_sv.py",
-    help="network architecture configuration file; the path must be relative to the current dir",
-)
-parser.add_argument(
-    "-o",
-    "--network-option",
-    nargs=2,
-    action="append",
-    default=[],
-    help="options to pass to the model class constructor, e.g., `--network-option use_counts False`",
-)
-parser.add_argument(
-    "-m",
-    "--model-prefix",
-    type=str,
-    default="models/{auto}/network",
-    help="path to save or load the model; for training, this will be used as a prefix, so model snapshots "
-    "will saved to `{model_prefix}_epoch-%d_state.pt` after each epoch, and the one with the best "
-    "validation metric to `{model_prefix}_best_epoch_state.pt`; for testing, this should be the full path "
-    "including the suffix, otherwise the one with the best validation metric will be used; "
-    "for training, `{auto}` can be used as part of the path to auto-generate a name, "
-    "based on the timestamp and network configuration",
-)
-parser.add_argument("--num-epochs", type=int, default=20, help="number of epochs")
-parser.add_argument(
-    "--steps-per-epoch",
-    type=int,
-    default=None,
-    help="number of steps (iterations) per epochs; if not set, each epoch will run over all loaded samples",
-)
-parser.add_argument(
-    "--steps-per-epoch-val",
-    type=int,
-    default=None,
-    help="number of steps (iterations) per epochs for validation; if not set, each epoch will run over all loaded samples",
-)
-parser.add_argument(
-    "--optimizer",
-    type=str,
-    default="ranger",
-    choices=["adam", "adamW", "ranger"],  # TODO: add more
-    help="optimizer for the training",
-)
-parser.add_argument(
-    "--optimizer-option",
-    nargs=2,
-    action="append",
-    default=[],
-    help="options to pass to the optimizer class constructor, e.g., `--optimizer-option weight_decay 1e-4`",
-)
-parser.add_argument(
-    "--lr-scheduler",
-    type=str,
-    default="flat+decay",
-    choices=["none", "steps", "flat+decay", "flat+linear", "flat+cos", "one-cycle"],
-    help="learning rate scheduler",
-)
-parser.add_argument(
-    "--load-epoch",
-    type=int,
-    default=None,
-    help="used to resume interrupted training, load model and optimizer state saved in the `epoch-%d_state.pt` and `epoch-%d_optimizer.pt` files",
-)
-parser.add_argument("--start-lr", type=float, default=5e-3, help="start learning rate")
-parser.add_argument("--batch-size", type=int, default=128, help="batch size")
-parser.add_argument(
-    "--use-amp",
-    action="store_true",
-    default=False,
-    help="use mixed precision training (fp16); NOT WORKING YET",
-)
-parser.add_argument(
-    "--gpus",
-    type=str,
-    default="0",
-    help='device for the training/testing; to use CPU, set to empty string (""); to use multiple gpu, set it as a comma separated list, e.g., `1,2,3,4`',
-)
-parser.add_argument(
-    "--num-workers",
-    type=int,
-    default=1,
-    help="number of threads to load the dataset; memory consumption and disk access load increases (~linearly) with this numbers",
-)
-parser.add_argument(
-    "--predict", action="store_true", default=False, help="run prediction instead of training"
-)
-parser.add_argument(
-    "--predict-output",
-    type=str,
-    help="path to save the prediction output, support `.root` and `.awkd` format",
-)
-parser.add_argument(
-    "--export-onnx",
-    type=str,
-    default=None,
-    help="export the PyTorch model to ONNX model and save it at the given path (path must ends w/ .onnx); "
-    "needs to set `--data-config`, `--network-config`, and `--model-prefix` (requires the full model path)",
-)
-parser.add_argument(
-    "--io-test", action="store_true", default=False, help="test throughput of the dataloader"
-)
-parser.add_argument(
-    "--copy-inputs",
-    action="store_true",
-    default=False,
-    help="copy input files to the current dir (can help to speed up dataloading when running over remote files, e.g., from EOS)",
-)
-parser.add_argument(
-    "--log",
-    type=str,
-    default="",
-    help="path to the log file; `{auto}` can be used as part of the path to auto-generate a name, based on the timestamp and network configuration",
-)
-parser.add_argument(
-    "--print",
-    action="store_true",
-    default=False,
-    help="do not run training/prediction but only print model information, e.g., FLOPs and number of parameters of a model",
-)
-parser.add_argument("--profile", action="store_true", default=False, help="run the profiler")
-
+parser.add_argument('--regression-mode', action='store_true', default=False,
+                    help='run in regression mode if this flag is set; otherwise run in classification mode')
+parser.add_argument('-c', '--data-config', type=str, default='data/ak15_points_pf_sv_v0.yaml',
+                    help='data config YAML file')
+parser.add_argument('-i', '--data-train', nargs='*', default=[],
+                    help='training files')
+parser.add_argument('-l', '--data-val', nargs='*', default=[],
+                    help='validation files; when not set, will use training files and split by `--train-val-split`')
+parser.add_argument('-t', '--data-test', nargs='*', default=[],
+                    help='testing files; supported syntax:'
+                         ' (a) plain list, `--data-test /path/to/a/* /path/to/b/*`;'
+                         ' (b) keyword-based, `--data-test a:/path/to/a/* b:/path/to/b/*`, will produce output_a, output_b;'
+                         ' (c) split output per N input files, `--data-test a%10:/path/to/a/*`, will split per 10 input files')
+parser.add_argument('--data-fraction', type=float, default=1,
+                    help='fraction of events to load from each file; for training, the events are randomly selected for each epoch')
+parser.add_argument('--file-fraction', type=float, default=1,
+                    help='fraction of files to load; for training, the files are randomly selected for each epoch')
+parser.add_argument('--fetch-by-files', action='store_true', default=False,
+                    help='When enabled, will load all events from a small number (set by ``--fetch-step``) of files for each data fetching. '
+                         'Otherwise (default), load a small fraction of events from all files each time, which helps reduce variations in the sample composition.')
+parser.add_argument('--fetch-step', type=float, default=0.01,
+                    help='fraction of events to load each time from every file (when ``--fetch-by-files`` is disabled); '
+                         'Or: number of files to load each time (when ``--fetch-by-files`` is enabled). Shuffling & sampling is done within these events, so set a large enough value.')
+parser.add_argument('--in-memory', action='store_true', default=False,
+                    help='load the whole dataset (and perform the preprocessing) only once and keep it in memory for the entire run')
+parser.add_argument('--train-val-split', type=float, default=0.8,
+                    help='training/validation split fraction')
+parser.add_argument('--demo', action='store_true', default=False,
+                    help='quickly test the setup by running over only a small number of events')
+parser.add_argument('--lr-finder', type=str, default=None,
+                    help='run learning rate finder instead of the actual training; format: ``start_lr, end_lr, num_iters``')
+parser.add_argument('--tensorboard', type=str, default=None,
+                    help='create a tensorboard summary writer with the given comment')
+parser.add_argument('--tensorboard-custom-fn', type=str, default=None,
+                    help='the path of the python script containing a user-specified function `get_tensorboard_custom_fn`, '
+                         'to display custom information per mini-batch or per epoch, during the training, validation or test.')
+parser.add_argument('-n', '--network-config', type=str, default='networks/particle_net_pfcand_sv.py',
+                    help='network architecture configuration file; the path must be relative to the current dir')
+parser.add_argument('-o', '--network-option', nargs=2, action='append', default=[],
+                    help='options to pass to the model class constructor, e.g., `--network-option use_counts False`')
+parser.add_argument('-m', '--model-prefix', type=str, default='models/{auto}/network',
+                    help='path to save or load the model; for training, this will be used as a prefix, so model snapshots '
+                         'will saved to `{model_prefix}_epoch-%d_state.pt` after each epoch, and the one with the best '
+                         'validation metric to `{model_prefix}_best_epoch_state.pt`; for testing, this should be the full path '
+                         'including the suffix, otherwise the one with the best validation metric will be used; '
+                         'for training, `{auto}` can be used as part of the path to auto-generate a name, '
+                         'based on the timestamp and network configuration')
+parser.add_argument('--num-epochs', type=int, default=20,
+                    help='number of epochs')
+parser.add_argument('--steps-per-epoch', type=int, default=None,
+                    help='number of steps (iterations) per epochs; '
+                         'if neither of `--steps-per-epoch` or `--samples-per-epoch` is set, each epoch will run over all loaded samples')
+parser.add_argument('--steps-per-epoch-val', type=int, default=None,
+                    help='number of steps (iterations) per epochs for validation; '
+                         'if neither of `--steps-per-epoch-val` or `--samples-per-epoch-val` is set, each epoch will run over all loaded samples')
+parser.add_argument('--samples-per-epoch', type=int, default=None,
+                    help='number of samples per epochs; '
+                         'if neither of `--steps-per-epoch` or `--samples-per-epoch` is set, each epoch will run over all loaded samples')
+parser.add_argument('--samples-per-epoch-val', type=int, default=None,
+                    help='number of samples per epochs for validation; '
+                         'if neither of `--steps-per-epoch-val` or `--samples-per-epoch-val` is set, each epoch will run over all loaded samples')
+parser.add_argument('--optimizer', type=str, default='ranger', choices=['adam', 'adamW', 'radam', 'ranger'],  # TODO: add more
+                    help='optimizer for the training')
+parser.add_argument('--optimizer-option', nargs=2, action='append', default=[],
+                    help='options to pass to the optimizer class constructor, e.g., `--optimizer-option weight_decay 1e-4`')
+parser.add_argument('--lr-scheduler', type=str, default='flat+decay',
+                    choices=['none', 'steps', 'flat+decay', 'flat+linear', 'flat+cos', 'one-cycle'],
+                    help='learning rate scheduler')
+parser.add_argument('--warmup-steps', type=int, default=0,
+                    help='number of warm-up steps, only valid for `flat+linear` and `flat+cos` lr schedulers')
+parser.add_argument('--load-epoch', type=int, default=None,
+                    help='used to resume interrupted training, load model and optimizer state saved in the `epoch-%d_state.pt` and `epoch-%d_optimizer.pt` files')
+parser.add_argument('--start-lr', type=float, default=5e-3,
+                    help='start learning rate')
+parser.add_argument('--batch-size', type=int, default=128,
+                    help='batch size')
+parser.add_argument('--use-amp', action='store_true', default=False,
+                    help='use mixed precision training (fp16)')
+parser.add_argument('--gpus', type=str, default='0',
+                    help='device for the training/testing; to use CPU, set to empty string (""); to use multiple gpu, set it as a comma separated list, e.g., `1,2,3,4`')
+parser.add_argument('--num-workers', type=int, default=1,
+                    help='number of threads to load the dataset; memory consumption and disk access load increases (~linearly) with this numbers')
+parser.add_argument('--predict', action='store_true', default=False,
+                    help='run prediction instead of training')
+parser.add_argument('--predict-output', type=str,
+                    help='path to save the prediction output, support `.root` and `.awkd` format')
+parser.add_argument('--export-onnx', type=str, default=None,
+                    help='export the PyTorch model to ONNX model and save it at the given path (path must ends w/ .onnx); '
+                         'needs to set `--data-config`, `--network-config`, and `--model-prefix` (requires the full model path)')
+parser.add_argument('--io-test', action='store_true', default=False,
+                    help='test throughput of the dataloader')
+parser.add_argument('--copy-inputs', action='store_true', default=False,
+                    help='copy input files to the current dir (can help to speed up dataloading when running over remote files, e.g., from EOS)')
+parser.add_argument('--log', type=str, default='',
+                    help='path to the log file; `{auto}` can be used as part of the path to auto-generate a name, based on the timestamp and network configuration')
+parser.add_argument('--print', action='store_true', default=False,
+                    help='do not run training/prediction but only print model information, e.g., FLOPs and number of parameters of a model')
+parser.add_argument('--profile', action='store_true', default=False,
+                    help='run the profiler')
 
 def to_filelist(args, mode="train"):
     if mode == "train":
