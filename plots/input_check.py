fd8cdebbce58c1612c6a356264c6788b83136871
cmantill <criss.ms7@gmail.com>
2022-03-23 12:52:35 -0500

    fix path

--- plots/input_check.py
+++ plots/input_check.py
@@ -17,14 +17,18 @@ os.system(f"mkdir -p {plot_dir}")
 for sample, (dir, sel) in samples.items():
     masks = {}
     nums = {}
-    print(f"{sample_dir}/*/{sel}*.root:{branch}")
-    events = uproot.iterate(f"{sample_dir}/*/{sel}*.root:{branch}",["fj_pt","fj_H_VV_4q","fj_nProngs"])
-    for ev in events:
-        masks["all"] = (events["fj_pt"] > 0)
-        if "all" in nums.keys():
-            nums["all"] += ak.sum(masks["all"])
-        else:
-            nums["all"] = ak.sum(masks["all"])
+    for tdir in ["train","test"]:
+        print(f"{sample_dir}/{tdir}/{dir}*/{sel}*.root:{branch}")
+        try:
+            events = uproot.iterate(f"{sample_dir}/{tdir}/{dir}*/{sel}*.root:{branch}",["fj_pt","fj_H_VV_4q","fj_nProngs"])
+            for ev in events:
+                masks["all"] = (events["fj_pt"] > 0)
+                if "all" in nums.keys():
+                    nums["all"] += ak.sum(masks["all"])
+                else:
+                    nums["all"] = ak.sum(masks["all"])
+        except:
+            print("No files in {tdir} for {sample}")
 
     num = nums['all']
     print(f"{sample}: num events {num}")
