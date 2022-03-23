4dc433cf56211681b1aa049aae97149017332b82
cmantill <criss.ms7@gmail.com>
2022-03-23 17:03:58 -0500

    move except

--- plots/input_check.py
+++ plots/input_check.py
@@ -30,31 +30,32 @@ for sample, (dir, sel) in samples.items():
                           "fj_isQCDothers"]
         try:
             events = uproot.iterate(f"{sample_dir}/{tdir}/{dir}*/{sel}*.root:{branch}",ibranches)
-            for ev in events:
-                masks = {}
-                masks["all"] = (ev["fj_pt"] > 0)
-                print(masks)
-                if "HH" in sample:
-                    masks["4q_all"] = masks["all"] & (ev["fj_H_VV_4q"] == 1)
-                    masks["4q"] = masks["4q_all"] & (ak.ev["fj_nprongs"] == 4)
-                    masks["3q"] = masks["4q_all"] & (ev["fj_nprongs"] == 3)
-                print(masks)
-                if "QCD" in sample:
-                    masks["qcdb"] = masks["all"] & (ev["fj_isQCDb"]==1)
-                    masks["qcdbb"] = masks["all"] & (ev["fj_isQCDbb"]==1)
-                    masks["qcdc"] = masks["all"] & (ev["fj_isQCDc"]==1)
-                    masks["qcdcc"] = masks["all"] & (ev["fj_isQCDcc"]==1)
-                    masks["qcdlep"] = masks["all"] & (ev["fj_isQCDlep"]==1)
-                    masks["qcdoth"] = masks["all"] & (ev["fj_isQCDothers"]==1)
-                    masks["all_tagged"] =  masks["qcdb"] | masks["qcdbb"] | masks["qcdc"] | masks["qcdcc"] | masks["qcdlep"] | masks["qcdoth"]
-                for m,mask in masks.items():
-                    print(m,ak.sum(mask))
-                    if m in nums.keys():
-                        nums[m] += ak.sum(mask)
-                    else:
-                        nums[m] = ak.sum(mask)
         except:
             print(f"No files in {tdir} for {sample}")
+            continue
+        
+        for ev in events:
+            masks = {}
+            masks["all"] = (ev["fj_pt"] > 0)
+            print(masks)
+            if "HH" in sample:
+                masks["4q_all"] = masks["all"] & (ev["fj_H_VV_4q"] == 1)
+                masks["4q"] = masks["4q_all"] & (ev["fj_nprongs"] == 4)
+                masks["3q"] = masks["4q_all"] & (ev["fj_nprongs"] == 3)
+            if "QCD" in sample:
+                masks["qcdb"] = masks["all"] & (ev["fj_isQCDb"]==1)
+                masks["qcdbb"] = masks["all"] & (ev["fj_isQCDbb"]==1)
+                masks["qcdc"] = masks["all"] & (ev["fj_isQCDc"]==1)
+                masks["qcdcc"] = masks["all"] & (ev["fj_isQCDcc"]==1)
+                masks["qcdlep"] = masks["all"] & (ev["fj_isQCDlep"]==1)
+                masks["qcdoth"] = masks["all"] & (ev["fj_isQCDothers"]==1)
+                masks["all_tagged"] =  masks["qcdb"] | masks["qcdbb"] | masks["qcdc"] | masks["qcdcc"] | masks["qcdlep"] | masks["qcdoth"]
+            for m,mask in masks.items():
+                print(m,ak.sum(mask))
+                if m in nums.keys():
+                    nums[m] += ak.sum(mask)
+                else:
+                    nums[m] = ak.sum(mask)
 
     if "all" in nums.keys():
         num = nums['all']
