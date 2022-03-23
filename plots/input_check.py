d6c4e869ded64c5e97bd2f449d4a9267f9c96085
cmantill <criss.ms7@gmail.com>
2022-03-23 12:49:47 -0500

    add printout

--- plots/input_check.py
+++ plots/input_check.py
@@ -17,6 +17,7 @@ os.system(f"mkdir -p {plot_dir}")
 for sample, (dir, sel) in samples.items():
     masks = {}
     nums = {}
+    print(f"{sample_dir}/*/{sel}*.root:{branch}")
     events = uproot.iterate(f"{sample_dir}/*/{sel}*.root:{branch}",["fj_pt","fj_H_VV_4q","fj_nProngs"])
     for ev in events:
         masks["all"] = (events["fj_pt"] > 0)
