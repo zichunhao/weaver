8ff667ed8a3268059182a0938b1c6d56c8de328b
cmantill <criss.ms7@gmail.com>
2022-05-06 10:58:15 -0500

    fixing bkglabels to be generic and getting rid of mbranch cut for wjets and tt

--- plots/labels.py
+++ plots/labels.py
@@ -1,8 +1,8 @@
 label_dict = {
     "qcd_old": {"legend": "QCD", "label": "fj_isQCD"},
-    "qcd": {"legend": "QCD", "label": "fj_QCD_label"},
-    "qcdnolep": {"legend": "QCD", "label": "fj_QCD_label"},
-    "qcd1lep": {"legend": "QCD", "label": "fj_QCD_label"},
+    "qcd": {"legend": "QCD", "label": "fj_bkg_label"},
+    "qcdnolep": {"legend": "QCD", "label": "fj_bkg_label"},
+    "qcd1lep": {"legend": "QCD", "label": "fj_bkg_label"},
 
     "top": {"legend": "Top", "label": "fj_isTop_label"},
     "top_lep": {"legend": "Top lep", "label": "fj_isToplep"},
@@ -11,11 +11,14 @@ label_dict = {
     "top_lepmerged": {"legend": "Top merged lepton", "label": "fj_isToplep_merged"},   
     "top_bsplit": {"legend": "Top unmerged b", "label": "fj_ttbar_bsplit"},
     "top_bmerged": {"legend": "Top merged b", "label": "fj_ttbar_bmerged"},
-    "ttbar": {"legend": "ttbar", "label": "fj_ttbar_label"},
+    
+    "ttbar": {"legend": "ttbar", "label": "fj_bkg_label"},
     
     "wjets": {"legend": "W+Jets", "label": "fj_wjets_label"},
-    "ttbarwjets": {"legend": "W+Jets and ttbar", "label": "fj_bkgd_label"},
     
+    "ttbarwjets": {"legend": "W+Jets and ttbar", "label": "fj_bkg_label"},
+    "ttbarwjetsqcd": {"legend": "W+Jets and ttbar and QCD", "label": "fj_bkg_label"},
+
     "hww_4q": {"legend": "H(VV) all-had", "label": "fj_H_VV_4q"},
     "hww_4q_merged": {"legend": "H(VV) 4q", "label": "fj_H_VV_4q_4q"},
     "hww_3q_merged": {"legend": "H(VV) 3q", "label": "fj_H_VV_4q_3q"},
